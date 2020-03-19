9daa581a9dc1fb181d91056926cbc534f7e3a687
Robert Lange <robert.lange.81@gmail.com>
2020-03-19 22:19:24 +0100

    create first entity and db migrations * php ./bin/console make:entity  > create classes * php ./bin/console doctrine:database:create  > add schema (use connection from .env) * php ./bin/console make:migration   > create migration files * php ./bin/console doctrine:migrations:migrate > execute mifrations

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -19,23 +19,91 @@ class CheeseListing
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255)
      */
-    private $CheeseListing2;
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getCheeseListing2(): ?string
+    public function getTitle(): ?string
     {
-        return $this->CheeseListing2;
+        return $this->title;
     }
 
-    public function setCheeseListing2(?string $CheeseListing2): self
+    public function setTitle(string $title): self
     {
-        $this->CheeseListing2 = $CheeseListing2;
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
 
         return $this;
     }
