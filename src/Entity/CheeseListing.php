8dea90636d3884fbe3bcf41e142282bc573b6a7c
Robert Lange <robert.lange.81@gmail.com>
2020-03-21 15:08:15 +0100

    add filters, for example: properties filter: curl -X GET "http://localhost:8000/api/cheeses?properties%5B%5D=title" -H "accept: application/ld+json"

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -2,7 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -20,6 +25,10 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
  *     shortName="cheeses"
  * )
  * @ORM\Entity(repositoryClass="App\Repository\CheeseListingRepository")
+ * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
+ * @ApiFilter(SearchFilter::class, properties={"title":"partial", "description":"partial"})
+ * @ApiFilter(RangeFilter::class, properties={"price"})
+ * @ApiFilter(PropertyFilter::class)
  */
 class CheeseListing
 {
@@ -80,6 +89,17 @@ class CheeseListing
     public function getDescription(): ?string
     {
         return $this->description;
+    }
+
+    /**
+     * @Groups("cheese_listing:read")
+     */
+    public function getShortDescription(): ?string
+    {
+        if(strlen($this->description) < 40)
+            return $this->description;
+
+        return substr($this->description, 0, 40) . "...";
     }
 
     public function setDescription($description): self
