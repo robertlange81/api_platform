b670c473263e01ca1035148c9a7fbbe09e071c3d
Robert Lange <robert.lange.81@gmail.com>
2020-07-23 23:34:17 +0200

    add global scss for yarn test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import CheeseWhizApp from './components/CheeseWhizApp';
 import 'bootstrap/dist/css/bootstrap.css';
+import '../scss/global.scss';
 
 Vue.component('cheese-whiz-app', CheeseWhizApp);
 
