7a6daf5a265fd1a9daac82d1f44b68e6d2a28505
Robert Lange <robert.lange.81@gmail.com>
2020-09-11 14:44:40 +0200

    use plain password for test

--- src/Test/CustomApiTestCase.php
+++ src/Test/CustomApiTestCase.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Test;
-
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
@@ -13,17 +11,21 @@ use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 class CustomApiTestCase extends ApiTestCase
 {
     use ReloadDatabaseTrait;
+
     protected function createUser(string $email, string $password): User
     {
         $user = new User();
         $user->setEmail($email);
         $user->setUsername(substr($email, 0, strpos($email, '@')));
-        $user->setPassword($password);
+        $encoded = self::$container->get('security.password_encoder')
+            ->encodePassword($user, $password);
+        $user->setPassword($encoded);
 
         // autowire doesn't work in test context !!! - get entity manager directly
         $em = self::$container->get(EntityManagerInterface::class);
         $em->persist($user);
         $em->flush();
+        $this->assertResponseStatusCodeSame(401);
         return $user;
     }
 
@@ -36,6 +38,12 @@ class CustomApiTestCase extends ApiTestCase
                 'password' => $password
             ],
         ]);
-        $this->assertResponseStatusCodeSame(204);
+    }
+
+    protected function createUserAndLogIn(Client $client, string $email, string $password): User
+    {
+        $user = $this->createUser($email, $password);
+        $this->logIn($client, $email, $password);
+        return $user;
     }
 }
